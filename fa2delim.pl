bdc90ed121c316774c5a4d7a8a8da074b1be6c7d
Samuel Shepard <vfn4@cdc.gov>
2016-04-15 12:46:38 -0400

    Added options to fa2delim.

--- fa2delim.pl
+++ fa2delim.pl
@@ -4,22 +4,27 @@
 
 use Getopt::Long;
 GetOptions( 
-		'csv-format|C' => \$CSV
+		'csv-format|C' => \$CSV,
+		'per-site|P' => \$perSite,
+		'enclose|E' => \$enclose,
+		'delimiter|D=s' => \$delim,
+		'extra-name|N=s' => \$name
 		);
 
 if ( -t STDIN && ! scalar(@ARGV) ) {
 	$message = "Usage:\n\tperl $0 <annotated.fasta> [options]\n";
-	$message = "\t\t-C|--csv-format\tUse csv delimiter (tab default).\n";
+	$message .= "\t\t-P|--per-site\tOne row per site.\n";
+	$message .= "\t\t-E|--enclose\tEnclose fields in quotes.\n";
+	$message .= "\t\t-C|--csv-format\tUse csv delimiter (tab default).\n";
+	$message .= "\t\t-D|--delimiter <CHAR>\tDelimiter for output.\n";
+	$message .= "\t\t-N|--extra-name <STR>\tExtra name field to add to every row.\n";
 	die($message."\n");
 }
-
 
 if ( defined($CSV) ) {
 	$delim = ',';
 	$enclose = 1;
-}
-
-if ( !defined($delim) ) {
+} elsif ( !defined($delim) ) {
 	$delim = "\t";
 	$enclose = 0;
 }
@@ -28,6 +33,12 @@ if ( $enclose ) {
 	$q = "'";
 } else {
 	$q = '';
+}
+
+if ( !defined($name) ) {
+	$extraField = "";
+} else {
+	$extraField = $delim.$q.$name.$q;
 }
 
 $/ = ">";
@@ -47,7 +58,15 @@ while( $record = <> ) {
 		$sequence =~ tr/'//d;
 	}
 
-	print $q,$header,$q,$delim,$q,$sequence,$q,"\n";
+	$length = length($sequence);
+
+	if ( $perSite ) {
+		for( $pos=0;$pos<$length;$pos++ ) {
+			print $q,$header,$q,$extraField,$delim,$q,($pos+1),$q,$delim,$q,substr($sequence,$pos,1),$q,"\n";
+		}
+	} else {
+		print $q,$header,$extraField,$q,$delim,$q,$sequence,$q,"\n";
+	}
 }
 
 # Trim function.

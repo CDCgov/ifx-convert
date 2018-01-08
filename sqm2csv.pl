4a7da6d65387821410720e2f8f3e9e887ef2afde
Samuel Shepard <vfn4@cdc.gov>
2018-01-08 13:47:18 -0500

    Fixed header issue with VS X

--- sqm2csv.pl
+++ sqm2csv.pl
@@ -5,16 +5,28 @@ use Getopt::Long;
 GetOptions( 	'csv|C=s'=> \$csv,
 		'use-index|I' => \$useIndex,
 		'dist-name|N=s'=>\$distName, 
-		'sample-set-name|S=s' => \$sampleSet,
+		'set-name|S=s' => \$sampleSet,
 		'vsX|V=s' => \$vsX,
-		'full-matrix|F' => \$fullMatrix
+		'full-matrix|F' => \$fullMatrix,
+		'vs-list|L=s' => \$vsList,
+		'remove-refs' => \$removeRefs,
+		'delim|D=s' => \$delim
 		);
 
 # VERSUS X
 # Annotation field
 # Strip annots from ID
 if( scalar(@ARGV) != 1 ) {
-	die("Usage:\n\tperl $0 <square_matrix.tab> [-C|--csv <file.csv>] [-I|--use-index] [-N|--dist-name <NAME>]\n");
+	$message = "Usage:\n\tperl $0 <square_matrix.tab>\n";
+	$message .= "\t\t-D|--delim\t\t\tDelimiter, default is TAB.\n";
+	$message .= "\t\t-C|--csv <file.csv>\t\tA different CSV to use for ID-pair order.\n";
+	$message .= "\t\t-I|--use-index\t\t\tUse numerical indices rather than the IDs.\n";
+	$message .= "\t\t-N|--dist-name <NAME>\t\tRename the distance header.\n";
+	$message .= "\t\t-S|--set-name\t\t\tAdd a column for the set name.\n";
+	$message .= "\t\t-V|--vsX <ID>\t\t\tOnly print versus ID.\n";
+	$message .= "\t\t-L|--vs-list <ID1[,ID2,..]>\tOne column is used per list reference (sqm subset).\n";
+	$message .= "\t\t-R|--remove-refs\t\tGiven above, remove reference rows.\n";
+	die($message."\n");
 }
 
 
@@ -22,6 +34,12 @@ if ( !defined($sampleSet) ) {
 	$sampleSet = '';
 } else {
 	$sampleSet = ",". $sampleSet;
+}
+
+if ( defined($delim) ) {
+	$d = $delim;
+} else {
+	$d = "\t";
 }
 
 open(IN,'<',$ARGV[0]) or die("Cannot open $ARGV[0].\n");
@@ -35,16 +53,30 @@ while($line = <IN>) {
 	@IDs[$i] = $id;
 	$i++;
 }
-close(IN);
-$N = $i;
-if ( !defined($distName) ) {
-	$distName = 'dist';
+close(IN); $N = $i;
+if ( !defined($distName) ) { $distName = 'dist'; }
+
+# process vs List
+if ( defined($vsList) ) {
+	%vsID = ();
+	foreach $id ( split(',',$vsList) ) {
+		if ( defined($indexByID{$id}) ) {
+			$vsID{$id} = $indexByID{$id};
+		} else {
+			print STDERR "Invalid reference, $id. Skipping.\n";
+		}
+	}
+	$vsList = 1;
+} else {
+	$vsList = 0;
 }
 
-if ( defined($vsX) ) {
-	print 'vsX,',$distName,"\n";
-} else {
-	print 'id1,id2,',$distName,"\n";
+if ( !$vsList ) {	
+	if ( defined($vsX) ) {
+		print "vs $vsX,$distName\n";
+	} else {
+		print "id1,id2,$distName\n";
+	}
 }
 if ( defined($csv) ) {
 	open(IN,'<', $csv) or die("Cannot open $csv.\n");
@@ -56,35 +88,50 @@ if ( defined($csv) ) {
 		$id2 = $values[1];
 
 		if ( !defined($vsX) ) {
-			print $id1,',',$id2,',',$distHash{$id1}[$indexByID{$id2}],$sampleSet,"\n";
+			print $id1,$d,$id2,$d,$distHash{$id1}[$indexByID{$id2}],$sampleSet,"\n";
 		} elsif( $id1 eq $vsX ) {
-			print $id2,',',$distHash{$id1}[$indexByID{$id2}],$sampleSet,"\n";
+			print $id2,$d,$distHash{$id1}[$indexByID{$id2}],$sampleSet,"\n";
 		} elsif( $id2 eq $vsX ) {
-			print $id1,',',$distHash{$id1}[$indexByID{$id2}],$sampleSet,"\n";
+			print $id1,$d,$distHash{$id1}[$indexByID{$id2}],$sampleSet,"\n";
 		}
 	}
 	close(IN);
 } else {
-	if ( $fullMatrix ) {
+	if ( $vsList ) {
+		@columns = sort { $a <=> $b } values(%vsID);
+		print 'ID'; 
+		if ( $distName ) { $suffix = '_'.$distName; } else { $suffix = ''; }
+		foreach $col ( @columns ) { print $d,$IDs[$col],$suffix; } 
+		print "\n";
+		for($i=0;$i<$N;$i++) {
+			if ( $removeRefs && defined($vsID{$IDs[$i]}) ) { next; }
+
+			print $IDs[$i];
+			foreach $col ( @columns ) {
+				print $d,$distHash{$IDs[$i]}[$col];
+			}
+			print "\n";
+		}
+	} elsif ( $fullMatrix ) {
 		for($i=0;$i<$N;$i++) {
 			for($j=0;$j<$N;$j++) {
 				if ( !defined($vsX ) ) {
 					if ( $useIndex ) {
-						print $i,',',$j,',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $i,$d,$j,$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					} else {
-						print $IDs[$i],',',$IDs[$j],',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $IDs[$i],$d,$IDs[$j],$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					}
 				} elsif( $IDs[$i] eq $vsX ) {
 					if ( $useIndex ) {
-						print $j,',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $j,$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					} else {
-						print $IDs[$j],',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $IDs[$j],$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					}
 				} elsif( $IDs[$j] eq $vsX ) {
 					if ( $useIndex ) {
-						print $i,',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $i,$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					} else {
-						print $IDs[$i],',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $IDs[$i],$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					}
 				}
 			}
@@ -94,21 +141,21 @@ if ( defined($csv) ) {
 			for($j=0;$j<$i;$j++) {
 				if ( !defined($vsX ) ) {
 					if ( $useIndex ) {
-						print $i,',',$j,',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $i,$d,$j,$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					} else {
-						print $IDs[$i],',',$IDs[$j],',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $IDs[$i],$d,$IDs[$j],$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					}
 				} elsif( $IDs[$i] eq $vsX ) {
 					if ( $useIndex ) {
-						print $j,',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $j,$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					} else {
-						print $IDs[$j],',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $IDs[$j],$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					}
 				} elsif( $IDs[$j] eq $vsX ) {
 					if ( $useIndex ) {
-						print $i,',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $i,$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					} else {
-						print $IDs[$i],',',$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
+						print $IDs[$i],$d,$distHash{$IDs[$i]}[$j],$sampleSet,"\n";
 					}
 				}
 			}

6cd0b91689779f6626379094341ddd3fa0ab02a2
Samuel Shepard <vfn4@cdc.gov>
2020-01-06 17:01:45 -0500

    Altered for string nulls (Impala style)

--- tab2parquet.py
+++ tab2parquet.py
@@ -16,12 +16,13 @@ output_parquet	= sys.argv[2]
 
 # Attribution: https://github.com/apache/arrow/blob/f7ef65e5fc367f1f5649dfcea0754e413fcca394/cpp/src/arrow/csv/options.cc#L28-L30
 # Added Impala-style null strings
-null_values = ["","#N/A","#N/AN/A","#NA","-1.#IND","-1.#QNAN","-NaN","-nan","1.#IND","1.#QNAN","N/A","NA","NULL","NaN","n/a","nan","null","\\N"]
+# Have to remove "NA" if strings can be NULL
+null_values = ["","#N/A","#N/AN/A","-1.#IND","-1.#QNAN","-NaN","-nan","1.#IND","1.#QNAN","N/A","NULL","NaN","n/a","nan","null","\\N"]
 
 try:
 	table = csv.read_csv(input_table, parse_options = csv.ParseOptions(delimiter='\t'), 
 		read_options = csv.ReadOptions(autogenerate_column_names = True),
-		convert_options = csv.ConvertOptions(null_values = null_values) ) 
+		convert_options = csv.ConvertOptions(null_values = null_values, strings_can_be_null = True) ) 
 		
 except IOError:
 	print("Error reading/parsing: " + input_table)

647071b14a9447a51ff8bc56795e23ab550e1ecd
Samuel Shepard <vfn4@cdc.gov>
2024-08-05 11:59:54 -0400

    Auto-format

--- tab2parquet.py
+++ tab2parquet.py
@@ -3,8 +3,9 @@
 # See defaults and options: https://arrow.apache.org/docs/python/index.html
 
 import sys
+
 if len(sys.argv) != 3:
-    print("Usage:\n\tpython "+sys.argv[0]+"\t<input.txt> <output.parquet>")
+    print("Usage:\n\tpython " + sys.argv[0] + "\t<input.txt> <output.parquet>")
     exit(1)
 
 
@@ -17,14 +18,34 @@ output_parquet = sys.argv[2]
 # Attribution: https://github.com/apache/arrow/blob/f7ef65e5fc367f1f5649dfcea0754e413fcca394/cpp/src/arrow/csv/options.cc#L28-L30
 # Added Impala-style null strings
 # Have to remove "NA" if strings can be NULL
-null_values = ["", "#N/A", "#N/AN/A", "-1.#IND", "-1.#QNAN", "-NaN", "-nan",
-               "1.#IND", "1.#QNAN", "N/A", "NULL", "NaN", "n/a", "nan", "null", "\\N"]
+null_values = [
+    "",
+    "#N/A",
+    "#N/AN/A",
+    "-1.#IND",
+    "-1.#QNAN",
+    "-NaN",
+    "-nan",
+    "1.#IND",
+    "1.#QNAN",
+    "N/A",
+    "NULL",
+    "NaN",
+    "n/a",
+    "nan",
+    "null",
+    "\\N",
+]
 
 try:
-    table = csv.read_csv(input_table, parse_options=csv.ParseOptions(delimiter='\t'),
-                         read_options=csv.ReadOptions(
-                             autogenerate_column_names=True),
-                         convert_options=csv.ConvertOptions(null_values=null_values, strings_can_be_null=True))
+    table = csv.read_csv(
+        input_table,
+        parse_options=csv.ParseOptions(delimiter="\t"),
+        read_options=csv.ReadOptions(autogenerate_column_names=True),
+        convert_options=csv.ConvertOptions(
+            null_values=null_values, strings_can_be_null=True
+        ),
+    )
 
 except IOError:
     print("Error reading/parsing: " + input_table)
